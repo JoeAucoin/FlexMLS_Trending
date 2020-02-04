a7b133dba3209c2c989af8c17e7d580cd229c60f
JoeAucoin <josephmaucoin@gmail.com>
2020-02-04 08:25:09 -0500

    Update to .Net 4.7.2

---
+++ README.md
@@ -0,0 +1,6 @@
+# FlexMLS_Trending Module
+FlexMLS Trending Module for DNN
+
+  [Contact developer with any questions or enhancement requests](https://www.gibs.com/Contact)
+
+  Visit working demo - [https://www.seashoreproperties.com/]
--- Settings.ascx.cs
+++ Settings.ascx.cs
@@ -7,6 +7,8 @@ using GIBS.Modules.FlexMLS.Components;
 using System.Collections.Generic;
 using System.Web.UI.WebControls;
 using DotNetNuke.Entities.Tabs;
+using System.Collections;
+using DotNetNuke.Services.Localization;
 
 namespace GIBS.Modules.FlexMLS_Trending
 {
@@ -23,10 +25,7 @@ namespace GIBS.Modules.FlexMLS_Trending
             {
                 if (!IsPostBack)
                 {
-
-                    GetTowns();
-                    GetMyTabs();
-
+                    BindModules();
          
                     if (Settings.Contains("Template"))
                     {
@@ -43,64 +42,7 @@ namespace GIBS.Modules.FlexMLS_Trending
                     {
                         txtNumberOfRecords.Text = Settings["NumberOfRecords"].ToString();
                     }
-
-                    if (Settings.Contains("PropertyType"))
-                    {
-                        ddlPropertyType.SelectedValue = Settings["PropertyType"].ToString();
-                    }
-
-                    if (Settings.Contains("Town"))
-                    {
-                        ddl_Town.SelectedValue = Settings["Town"].ToString();
-                    }
-
-                    if (Settings.Contains("Village"))
-                    {
-                        ddl_Village.SelectedValue = Settings["Village"].ToString();
-                    }
-
-                    if (Settings.Contains("Bedrooms"))
-                    {
-                        ddlBedRooms.SelectedValue = Settings["Bedrooms"].ToString();
-                    }
-
-                    if (Settings.Contains("Bathrooms"))
-                    {
-                        ddlBathRooms.SelectedValue = Settings["Bathrooms"].ToString();
-                    }
-
-                    if (Settings.Contains("PriceLow"))
-                    {
-                        ddlPriceLow.SelectedValue = Settings["PriceLow"].ToString();
-                    }
-
-                    if (Settings.Contains("PriceHigh"))
-                    {
-                        ddlPriceHigh.SelectedValue = Settings["PriceHigh"].ToString();
-                    }
-
-                    if (Settings.Contains("WaterFront"))
-                    {
-                        if (Settings["WaterFront"].ToString().Length > 0)
-                        cbxWaterFront.Checked = Convert.ToBoolean(Settings["WaterFront"].ToString());
-                    }
-
-                    if (Settings.Contains("Waterview"))
-                    {
-                        if (Settings["Waterview"].ToString().Length > 0)
-                        cbxWaterView.Checked = Convert.ToBoolean(Settings["Waterview"].ToString());
-                    }
-
-                    if (Settings.Contains("Complex"))
-                    {
-                        ddlComplex.SelectedValue = Settings["Complex"].ToString();
-                    }
-
-                    if (Settings.Contains("DOM"))
-                    {
-                        ddlDOM.SelectedValue = Settings["DOM"].ToString();
-                    }
-
+                                        
                     if (Settings.Contains("FlexMLSPage"))
                     {
                         ddlViewListing.SelectedValue = Settings["FlexMLSPage"].ToString();
@@ -146,65 +88,44 @@ namespace GIBS.Modules.FlexMLS_Trending
             }
         }
 
-        public void GetMyTabs()
+
+        private void BindModules()
+
         {
 
-            try
+            DotNetNuke.Entities.Modules.ModuleController mc = new ModuleController();
+            ArrayList existMods = mc.GetModulesByDefinition(this.PortalId, "GIBS - FlexMLS");
+
+            foreach (DotNetNuke.Entities.Modules.ModuleInfo mi in existMods)
+
             {
-                ddlViewListing.DataSource = TabController.GetPortalTabs(this.PortalId, this.TabId, true, false);
-                ddlViewListing.DataBind();
+                if (!mi.IsDeleted)
+                {
+                    DotNetNuke.Entities.Tabs.TabController tabController = new DotNetNuke.Entities.Tabs.TabController();
+                    DotNetNuke.Entities.Tabs.TabInfo tabInfo = tabController.GetTab(mi.TabID, this.PortalId);
+
+                    string strPath = tabInfo.TabName.ToString();
+
+                    ListItem objListItem = new ListItem();
+
+                    objListItem.Value = mi.TabID.ToString();     // TabID & ModuleID      + "-" + mi.ModuleID.ToString()
+                    objListItem.Text = strPath + " -> " + mi.ModuleTitle.ToString();
+                                       
+                    ddlViewListing.Items.Add(objListItem);
+                }
             }
-            catch (Exception ex)
-            {
-                Exceptions.ProcessModuleLoadException(this, ex);
-            }
+
+            ddlViewListing.Items.Insert(0, new ListItem(Localization.GetString("SelectModule", this.LocalResourceFile), "-1"));
 
         }
 
-        public void GetTowns()
-        {
-
-            try
-            {
-
-                List<FlexMLSInfo> items;
-
-                FlexMLSController controller = new FlexMLSController();
-                items = controller.FlexMLS_Lookup_Town();
 
 
-                ddl_Town.DataSource = items;
-                ddl_Town.DataTextField = "Town";
-                ddl_Town.DataValueField = "Town";
-                ddl_Town.DataBind();
-
-                ddl_Town.Items.Insert(0, new ListItem("--Select--", ""));
-
-            }
-            catch (Exception ex)
-            {
-                Exceptions.ProcessModuleLoadException(this, ex);
-            }
-
-        }
-
-        protected void ddl_Town_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-            List<FlexMLSInfo> items;
-            FlexMLSController controller = new FlexMLSController();
-
-            items = controller.FlexMLS_Lookup_Village(ddl_Town.SelectedValue.ToString());
-
-            ddl_Village.DataSource = items;
-            ddl_Village.DataTextField = "Village";
-            ddl_Village.DataValueField = "Village";
-            ddl_Village.DataBind();
-
-            ddl_Village.Items.Insert(0, new ListItem("--Optionally Select--", ""));
 
 
-        }
+
+
+
 
     }
 }
